5f25422fe1c6504d2999518e6d549cce1c6d5a81
Zoltan Kochan <z@kochan.io>
2018-02-24 13:36:09 +0200

    test: add test for exclude pattern

--- test/index.ts
+++ test/index.ts
@@ -27,3 +27,17 @@ test('finds packages by patterns', async t => {
   t.ok(pkgs[0].manifest.name, 'component-2')
   t.end()
 })
+
+test('ignore packages by patterns', async t => {
+  const root = path.join(fixtures, 'many-pkgs')
+  const pkgs = await findPackages(root, {patterns: ['**', '!libs/**']})
+
+  t.equal(pkgs.length, 2)
+  t.ok(pkgs[0].path)
+  t.ok(pkgs[0].manifest)
+  t.ok(pkgs[0].manifest.name, 'component-1')
+  t.ok(pkgs[1].path)
+  t.ok(pkgs[1].manifest)
+  t.ok(pkgs[0].manifest.name, 'component-2')
+  t.end()
+})
